590c17b565e0bcb8cbbad048a89dc500afb5c28a
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2024-06-23 16:07:20 -0400

    Options Menu basic logic

--- app/src/main/java/com/comp304/lab2/HomeTypeSelectionActivity.kt
+++ app/src/main/java/com/comp304/lab2/HomeTypeSelectionActivity.kt
@@ -1,6 +1,10 @@
 package com.comp304.lab2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,49 @@ class HomeTypeSelectionActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.home_types_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_apartment -> {
+                val intent = Intent(this, ApartmentActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.menu_detached_home -> {
+                val intent = Intent(this, DetachedHomeActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.menu_semi_detached_home -> {
+                val intent = Intent(this, SemiDetachedHomeActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.menu_condominium_apartment -> {
+                val intent = Intent(this, CondominiumActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.menu_townhouse -> {
+                val intent = Intent(this, TownHouseActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/comp304/lab2/MainActivity.kt
+++ app/src/main/java/com/comp304/lab2/MainActivity.kt
@@ -4,6 +4,8 @@ package com.comp304.lab2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        supportActionBar?.hide()
 
     }
 }
