ad08af6d853ffa3309a933e95d1dd9e28a57883d
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2024-06-23 06:49:57 -0400

    Added main to selection type activity logic

--- app/src/main/java/com/comp304/lab2/MainActivity.kt
+++ app/src/main/java/com/comp304/lab2/MainActivity.kt
@@ -2,7 +2,9 @@
 
 package com.comp304.lab2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,5 +20,15 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Find button and set OnClickListener
+        // to move to home type selection activity
+        val button: Button = findViewById(R.id.main_button)
+        button.setOnClickListener {
+            val intent = Intent(this, HomeTypeSelectionActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
